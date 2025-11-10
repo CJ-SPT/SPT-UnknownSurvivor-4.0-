86dff5dc27de2cee7d37aed599567e2e27ea6d63
Doup22 <dustinsnyder31@gmail.com>
2025-11-09 21:16:16 -0500

    Plugin.cs fix

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -3,16 +3,16 @@ using HarmonyLib;
 
 namespace SurvivorQuestExtension
 {
-    [BepInPlugin("com.yourname.SurvivorQuestExtension", "Survivor Quest Extension", "1.0.0")]
+    [BepInPlugin("com.dsnyder.SurvivorQuestExtension", "Survivor Quest Extension", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
         void Awake()
         {
             try
             {
-                var harmony = new Harmony("com.yourname.SurvivorQuestExtension");
+                var harmony = new Harmony("com.dsnyder.SurvivorQuestExtension");
                 harmony.PatchAll();
-                Logger.LogInfo("Survivor Quest Extension loaded successfully!");
+                Logger<>.LogInfo("Survivor Quest Extension loaded successfully!");
             }
             catch (System.Exception ex)
             {
