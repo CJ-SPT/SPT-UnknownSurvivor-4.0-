76d37aa493d16365eedf672fc1a9b18a14b89d94
Doup22 <dustinsnyder31@gmail.com>
2025-11-09 21:12:50 -0500

    Client Mod portion added and repo restructure

---
+++ Client/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace SurvivorQuestExtension
+{
+    [BepInPlugin("com.yourname.SurvivorQuestExtension", "Survivor Quest Extension", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            try
+            {
+                var harmony = new Harmony("com.yourname.SurvivorQuestExtension");
+                harmony.PatchAll();
+                Logger.LogInfo("Survivor Quest Extension loaded successfully!");
+            }
+            catch (System.Exception ex)
+            {
+                Logger.LogError($"Failed to load: {ex}");
+            }
+        }
+    }
+}
